f4198bb0246f85999ef311d9df92e2128454b7ce
Xasan <Xasanyolchiyev@gmail.com>
2024-03-05 12:12:08 +0500

    add index.html

---
+++ hw.js
@@ -0,0 +1,4 @@
+const array = [5,22,44,67]
+array.sort((a,b)=>b-a )
+let slice = array.slice(0,2)
+console.log(slice);
---
+++ main.js
@@ -0,0 +1,5 @@
+const array = []
+let userAnswer = prompt("write whatever you want")
+let arr = userAnswer.split("")
+let slice = userAnswer.slice(1,-1)
+console.log(slice);
